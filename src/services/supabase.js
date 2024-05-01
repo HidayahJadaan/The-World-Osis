dbccc2c63c9ac66d689850dbf12056660f0283ed
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2024-05-01 18:11:45 +0300

    Supabase Key Updated..

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,17 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://pisusqrhcotbkawpoasg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpc3VzcXJoY290Ymthd3BvYXNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0Nzg2MjgsImV4cCI6MjAzMDA1NDYyOH0.hwXszZ1rWCkI1O7jnuTKxx4FIkJQPd9Ch--gkLRdZ_0'
-const supabase = createClient(supabaseUrl, supabaseKey)
-
+export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpc3VzcXJoY290Ymthd3BvYXNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0Nzg2MjgsImV4cCI6MjAzMDA1NDYyOH0.hwXszZ1rWCkI1O7jnuTKxx4FIkJQPd9Ch--gkLRdZ_0";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+
+// import { createClient } from '@supabase/supabase-js'
+
+// const supabaseUrl = 'https://pisusqrhcotbkawpoasg.supabase.co'
+// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpc3VzcXJoY290Ymthd3BvYXNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0Nzg2MjgsImV4cCI6MjAzMDA1NDYyOH0.hwXszZ1rWCkI1O7jnuTKxx4FIkJQPd9Ch--gkLRdZ_0'
+// const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+// export default supabase;
